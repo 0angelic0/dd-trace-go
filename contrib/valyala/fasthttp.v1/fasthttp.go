9055eff2bd5378ee43d18ec9f06836f46cc4b868
Pisit (King) Tangkoblarp <pisit.t@t2pco.com>
2024-11-01 11:11:48 +0700

    add: log to be know this repo is using

--- contrib/valyala/fasthttp.v1/fasthttp.go
+++ contrib/valyala/fasthttp.v1/fasthttp.go
@@ -34,6 +34,7 @@ func WrapHandler(h fasthttp.RequestHandler, opts ...Option) fasthttp.RequestHand
 	}
 	log.Debug("contrib/valyala/fasthttp.v1: Configuring Middleware: cfg: %#v", cfg)
 	log.Info("0angelic0 version")
+	fmt.Println("WrapHandler 0angelic0 version")
 	return func(fctx *fasthttp.RequestCtx) {
 		if cfg.ignoreRequest(fctx) {
 			h(fctx)
